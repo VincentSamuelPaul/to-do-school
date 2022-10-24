f6fc9f0ff16d9283b9a710791ccf8891034dd92b
Vincent Paul <90044187+VincentSamuelPaul@users.noreply.github.com>
2022-10-24 20:34:08 +0530

    final

--- backend/settings.py
+++ backend/settings.py
@@ -121,7 +121,8 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000',
-    'https://taskmanagercs.netlify.app',
-    )
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:3000',
+#     'https://taskmanagercs.netlify.app',
+#     )
